1ea75fa6e414cfc13babe8e209cd9eaa77f19fd6
shinich39 <shinich39@gmail.com>
2025-12-14 05:26:30 +0900

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -13,7 +13,6 @@ export default [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       '@typescript-eslint/prefer-as-const': 'off',
-      "@typescript-eslint/no-empty-object-type": "off",
     },
   },
 ];
