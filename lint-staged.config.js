6f1c6e2499edc69b9ff64d48d7d44a1ef5e43248
shinich39 <shinich39@gmail.com>
2026-02-25 07:38:24 +0900

    fix

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,7 @@
 /** @type {import('lint-staged').Configuration} */
 export default {
-  "src/**/*.{ts,tsx,js}": (files) => 
-    `eslint ${files.filter((file) => !/\.test\.(?:ts|tsx|js)$/.test(file)).join(' ')}`,
+  "src/**/*.{ts,tsx,js}": (files) => {
+    const targets = files.filter((file) => !/\.test\.(?:ts|tsx|js)$/.test(file));
+    return targets.length === 0 ? [] : `eslint ${targets.join(' ')}`;
+  }
 }
