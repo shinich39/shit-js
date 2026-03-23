53c9033f7234484f2aa48d25b4507696e91df225
shinich39 <shinich39@gmail.com>
2026-03-24 06:08:09 +0900

    fix

--- src/modules/create-i18n.ts
+++ src/modules/create-i18n.ts
@@ -7,10 +7,10 @@
  *   ko: { heading: "세상아, 안녕!" }
  * }, "en");
  *
- * t("en", "heading"); // "Hello, world!"
- * t(null, "heading"); // "Hello, world!"
- * t("ko", "heading"); // "세상아, 안녕!"
- * t("en", "missing"); // "missing"
+ * t("heading"); // "Hello, world!"
+ * t("heading", "en"); // "Hello, world!"
+ * t("heading", "ko"); // "세상아, 안녕!"
+ * t("missing", "en"); // "missing"
  */
 export function createI18n(
   obj: Record<string, Record<string, string>>,
